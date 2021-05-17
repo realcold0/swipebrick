4bfdbd95f2cc35a585fe94a2d4e1a4a324086279
realcold0 <65608503+realcold0@users.noreply.github.com>
2021-05-17 22:28:24 +0900

    플레이어 생성 및 이동

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Vector3 firstPos, secPos, gap;
+    public GameObject player;
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            firstPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            secPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            gap = secPos - firstPos;
+            Instantiate(player, new Vector3(0,-3.94f,0), Quaternion.identity);
+        }
+
         
     }
+    
 }
---
+++ Assets/Scenes/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    Player player = GameObject.Find("BallSpawn").GetComponent<Player>();
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,1,0 );
+
+    }
+}
